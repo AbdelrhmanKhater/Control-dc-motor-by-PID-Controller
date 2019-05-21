b3dae3171ad1e8eae9c771e00386070585c9ab10
ak7 <abdelrhman.khater111@gmail.com>
2019-05-21 23:56:34 +0200

    First commit

---
+++ pos.ino
@@ -0,0 +1,130 @@
+#define encoder0PinA  2
+#define encoder0PinB  3
+#define PWM 5
+//#define direction_input A0
+#define pos_in A0 
+#define dir1 6
+#define dir2 7
+#define max_rpm 290
+int sp = 0;
+volatile signed int encoder0Pos = 0;
+int v = 0;
+int s = 0;
+int e = 0;
+int cnt = 0;
+int number_of_cycles = 48;
+int Dir= 1;
+int s_dir = -1;
+int factor;
+float err, err_prev, total_err, changeErr;
+float kp, ki, kd;
+int desired;
+int pidTerm, pidTerm_scaled;
+
+void PIDcalculation(){
+  desired = (((4 * 3.14) * analogRead(pos_in))/2046) - ((2 * 3.14) / 3);
+  err = desired - encoder0Pos;
+  
+  changeErr = err - err_prev; // derivative term
+  total_err += err; //accumalate errors to find integral term
+  pidTerm = (kp * err) + (ki * total_err) + (kd * changeErr);//total gain
+  pidTerm = constrain(pidTerm, -255, 255);//constraining to appropriate value
+  pidTerm_scaled = abs(pidTerm);//make sure it's a positive value
+
+  err_prev = err;
+}
+void setup() {
+  //////////speed 
+  
+  //factor =(sp*255)/(max_rpm);
+  //pinMode(PWM, OUTPUT);
+  total_err = 0;
+  err_prev = 0;
+  kp = 0.8;
+  ki = 0.2;
+  kd = 0.4;
+  //analogWrite(PWM, factor);
+  pinMode(dir1,OUTPUT);
+  //digitalWrite(direction_output1, HIGH);
+  pinMode(dir2,OUTPUT);
+  //digitalWrite(direction_output2, LOW);
+  /////////////////////////////
+  pinMode(encoder0PinA, INPUT);
+  digitalWrite(encoder0PinA, HIGH);       // turn on pull-up resistor
+  pinMode(encoder0PinB, INPUT);
+  digitalWrite(encoder0PinB, HIGH);       // turn on pull-up resistor
+
+  attachInterrupt(0, doEncoder_Expanded, RISING);  // encoder pin on interrupt 0 - pin 2
+  Serial.begin (9600);
+  Serial.println("start");                // a personal quirk
+}
+
+void loop() {
+  // do some stuff here - the joy of interrupts is that they take care of themselves
+  PIDcalculation();
+   if (encoder0Pos < desired) {
+    digitalWrite(dir1, LOW);// Forward motion
+    digitalWrite(dir2, HIGH);
+  } else {
+    digitalWrite(dir1, HIGH);//Reverse motion
+    digitalWrite(dir2, LOW);
+  }
+
+  analogWrite(PWM, pidTerm_scaled);
+ 
+}
+
+void doEncoder() {
+  /* If pinA and pinB are both high or both low, it is spinning
+     forward. If they're different, it's going backward.
+
+     For more information on speeding up this process, see
+     [Reference/PortManipulation], specifically the PIND register.
+  */
+  cnt = cnt + 1;
+  cnt = cnt % number_of_cycles;
+  if (digitalRead(encoder0PinA) == digitalRead(encoder0PinB)) {
+    s_dir = 0;
+  } else {
+    s_dir = 1;
+  }
+  if (cnt == 0)
+    s = millis();
+ if (cnt == number_of_cycles - 1)
+ {
+  e = millis();
+  int l = 60000;
+  v = l / (e - s);    
+  Serial.println(v, DEC); 
+ }
+  Serial.println (encoder0Pos, DEC);
+}
+
+/* See this expanded function to get a better understanding of the
+   meanings of the four possible (pinA, pinB) value pairs:
+*/
+void doEncoder_Expanded() {
+  if (digitalRead(encoder0PinA) == HIGH) {   // found a low-to-high on channel A
+    if (digitalRead(encoder0PinB) == LOW) {  // check channel B to see which way
+      // encoder is turning
+      encoder0Pos = encoder0Pos - 1;         // CCW
+    }
+    else {
+      encoder0Pos = encoder0Pos + 1;         // CW
+    }
+  }
+  else if(digitalRead(encoder0PinA) == LOW)                                        // found a high-to-low on channel A
+  {
+    if (digitalRead(encoder0PinB) == LOW) {   // check channel B to see which way
+      // encoder is turning
+      encoder0Pos = encoder0Pos + 1;          // CW
+    }
+    else {
+      encoder0Pos = encoder0Pos - 1;          // CCW
+    }
+
+  }
+  Serial.println (encoder0Pos, DEC);          // debug - remember to comment out
+  // before final program run
+  // you don't want serial slowing down your program if not needed
+}
